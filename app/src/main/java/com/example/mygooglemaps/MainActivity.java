1071b36e802c7620672832388ec3ba976edee1de
julio <guty312005@gmail.com>
2025-10-23 08:50:16 -0400

    Actualización con los últimos cambios

--- app/src/main/java/com/example/mygooglemaps/MainActivity.java
+++ app/src/main/java/com/example/mygooglemaps/MainActivity.java
@@ -37,10 +37,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng santaCruz=new LatLng(-17.345692, -63.246626);
         //myMap.addMarker(new MarkerOptions().position(santaCruz).title("Santa Cruz Elay"));
         myMap.moveCamera(CameraUpdateFactory.newLatLng(santaCruz));
-        MarkerOptions options = new MarkerOptions().position(santaCruz).title("Santa Cruz My casa");
+        MarkerOptions options = new MarkerOptions().position(santaCruz).title("Mi Casa").snippet("Santa Cruz, Bolivia - Mi hogar");
         options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
         myMap.addMarker(options);
 
+        // familia Almanza tía
+        LatLng familiaAlmanza = new LatLng(-17.346832, -63.391464);
+        MarkerOptions almanzaOptions = new MarkerOptions().position(familiaAlmanza).title("Familia Almanza - Tía").snippet("Familia materna - Santa Cruz");
+        almanzaOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        myMap.addMarker(almanzaOptions);
+
+        // familia Guzmán abuelo
+        LatLng familiaGuzman = new LatLng(-17.009756, -63.233895);
+        MarkerOptions guzmanOptions = new MarkerOptions().position(familiaGuzman).title("Familia Guzmán - Abuelo").snippet("Familia paterna - Santa Cruz");
+        guzmanOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        myMap.addMarker(guzmanOptions);
+
+        // hermano Wilmer
+        LatLng hermanoWilmer = new LatLng(-17.336272329903196, -63.26324270349139);
+        MarkerOptions wilmerOptions = new MarkerOptions().position(hermanoWilmer).title("Hermano Wilmer").snippet("Hermano - Santa Cruz");
+        wilmerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+        myMap.addMarker(wilmerOptions);
+
+        // hermana Wendy
+        LatLng hermanaWendy = new LatLng(-17.341766913831528, -63.24877768243336);
+        MarkerOptions wendyOptions = new MarkerOptions().position(hermanaWendy).title("Hermana Wendy").snippet("Hermana - Santa Cruz");
+        wendyOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+        myMap.addMarker(wendyOptions);
+
+        // casa de tía Claudia
+        LatLng tiaClaudia = new LatLng(38.780121, -77.464774);
+        MarkerOptions claudiaOptions = new MarkerOptions().position(tiaClaudia).title("Casa de Tía Claudia").snippet("Ubicación en Estados Unidos");
+        claudiaOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+        myMap.addMarker(claudiaOptions);
     }
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
